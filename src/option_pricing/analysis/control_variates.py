48bf34d68c5d8ee39356e200cf6f6fa635148584
marvinsohn <marvinsohn22@gmail.com+>
2023-03-30 21:44:34 +0200

    Insert missing input parameters into cv_european_vanilla if the function should use the delta control variate.

--- src/option_pricing/analysis/control_variates.py
+++ src/option_pricing/analysis/control_variates.py
@@ -72,7 +72,19 @@ def cv_european_vanilla(
 
     elif variate == "delta":
 
-        price, standard_error = cv_european_vanilla_delta()
+        price, standard_error = cv_european_vanilla_delta(
+            s0=s0,
+            k=k,
+            r=r,
+            t=t,
+            sigma=sigma,
+            option_type=option_type,
+            number_steps=number_steps,
+            number_replications=number_replications,
+            nu_dt=nu_dt,
+            sigma_sqrt_dt=sigma_sqrt_dt,
+            dt=dt,
+        )
 
     return price, standard_error
 
