860ec2b8a29ce18401573778a245e56270ccc080
marvinsohn <marvinsohn22@gmail.com+>
2023-03-30 22:54:08 +0200

    Remove axis argument from get_gamma function.

--- src/option_pricing/analysis/control_variates.py
+++ src/option_pricing/analysis/control_variates.py
@@ -428,12 +428,10 @@ def cv_european_vanilla_all(
     # Third step: Gamma variate for perfectly negative correlated assets
     gamma_underlying = get_gamma(
         s0=st_underlying[1:] - st_underlying[:-1] * np.exp(r * dt),
-        axis=0,
     )
     gamma_negatively_correlated = get_gamma(
         s0=st_negatively_correlated[1:]
         - st_negatively_correlated[:-1] * np.exp(r * dt),
-        axis=0,
     )
     gamma_underlying_list = np.cumsum(
         gamma_underlying
